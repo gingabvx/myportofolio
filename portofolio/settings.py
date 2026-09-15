1ad24e816b849db5d204dc77cc0e5ca88bd4c6b2
narendrarama05 <narendraramaprawira@gmail.com>
2026-09-15 13:11:52 +0700

    test

--- portofolio/settings.py
+++ portofolio/settings.py
@@ -153,3 +153,6 @@ MAILERS = {
         'BACKEND': 'django.core.mail.backends.console.EmailBackend',
     },
 }
+
+# gunakan https:// untuk trailing urlnya
+CSRF_TRUSTED_ORIGINS = ["https://narendra-rama51-myportofolio.pws.cs.ui.ac.id/"]

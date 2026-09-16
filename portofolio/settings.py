2199b906ab68adab3bc2986a44a24894e863cf3f
gingabvx <narendraramaprawira@gmail.com>
2026-09-16 13:38:52 +0700

    fix: Fix csrf?

--- portofolio/settings.py
+++ portofolio/settings.py
@@ -155,4 +155,4 @@ MAILERS = {
 }
 
 # gunakan https:// untuk trailing urlnya
-CSRF_TRUSTED_ORIGINS = ["https://narendra-rama51-myportofolio.pws.cs.ui.ac.id/"]
+CSRF_TRUSTED_ORIGINS = ["https://narendra-rama51-myportofolio.pws.cs.ui.ac.id"]
